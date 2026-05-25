a3bff98e4c7bfd74796a660f2b343694eec217bf
VV <3353695529@qq.com>
2026-05-26 01:04:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("steamLogin")
 .onclick = () => {
-    window.location.href =
+    window.location.href =brilliantbusinessadministrationbureau-production.up.railway.app
     "https://你的后端地址/auth/steam";
 };
